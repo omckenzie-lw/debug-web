f5d0e4259af2fec8534f4122824c7da29471154e
OM <omar.mckenzie@lucidworks.com>
2025-12-02 17:47:04 +0000

    Squashed commit of the following:
    commit e104ccc4cc29e47a62cd9bf80d89aa5009e6690a
    Author: Omar M <74363335+omckenzie-lw@users.noreply.github.com>
    Date:   Tue Dec 2 17:45:20 2025 +0000
    
        Update README.md
    
    commit de03c3b19312d817f14d40e14e7261c633386bdf
    Author: Omar M <74363335+omckenzie-lw@users.noreply.github.com>
    Date:   Tue Dec 2 17:40:51 2025 +0000
    
        x
    
    commit 01eb70e66eddd83ad44f9fdc577629d4f19b4a3f
    Author: Omar M <74363335+omckenzie-lw@users.noreply.github.com>
    Date:   Tue Dec 2 17:33:07 2025 +0000
    
        x
    
    commit ff7830a14c547c06b8d98e892085daae4df58b89
    Author: Omar M <74363335+omckenzie-lw@users.noreply.github.com>
    Date:   Tue Dec 2 17:18:15 2025 +0000
    
        x
    
    commit 2aa6f13870fad0d8699c354bded0fd69bad66c13
    Author: Omar M <74363335+omckenzie-lw@users.noreply.github.com>
    Date:   Tue Dec 2 16:52:52 2025 +0000
    
        i
    
    commit 89cb1e2f0a5a67c8a141abe6c928dcbe39af58ca
    Author: Omar M <74363335+omckenzie-lw@users.noreply.github.com>
    Date:   Tue Dec 2 16:49:51 2025 +0000
    
        i

---
+++ sust-1454/app.py
@@ -0,0 +1,62 @@
+import os
+import random
+import string
+from datetime import datetime, timedelta
+from flask import Flask, jsonify, request, make_response
+
+app = Flask(__name__)
+
+USER = os.getenv('API_USER', 'username')
+PASS = os.getenv('API_PASS', 'password')  # Set your password in environment
+
+
+def validate_credentials(auth):
+    return auth and auth.username == USER and auth.password == PASS
+
+
+def random_string(prefix, length=6):
+    return prefix + ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
+
+
+def random_date():
+    start_date = datetime(2010, 1, 1)
+    end_date = datetime.now()
+    delta = end_date - start_date
+    random_days = random.randint(0, delta.days)
+    return (start_date + timedelta(days=random_days)).strftime('%d %b %Y')
+
+
+def random_url():
+    base = "https://www.fake.com/resource/en/datasheet/"
+    filename = random_string('st', 8) + ".pdf"
+    return base + filename
+
+
+@app.route('/api/v1/filemetadata', methods=['GET'])
+def file_metadata():
+    auth = request.authorization
+    if not validate_credentials(auth):
+        resp = make_response('Authentication required', 401)
+        resp.headers['WWW-Authenticate'] = 'Basic realm="Secure Area"'
+        return resp
+
+    results = []
+    for i in range(5000):
+        id = random_url()
+        data = {
+            "alternate_name_s": random_string('ds'),
+            "associationType_s": random.choice(["Datasheet", "Manual", "Specification"]),
+            "id": id,
+            "intentDocumentType_s": random.choice(["Technical Documents", "User Guide", "Reference Manual"]),
+            "last_modification_date_s": random_date(),
+            "locale_s": random.choice(["en", "fr", "de", "it"]),
+            "counter_i": i
+        }
+        results.append(data)
+    return jsonify(results)
+
+
+if __name__ == '__main__':
+    # For local HTTPS, generate cert.pem and key.pem using OpenSSL
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
